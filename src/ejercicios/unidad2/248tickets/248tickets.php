c38a433d9c976d3c721e717b0602d053994c0162
Juan Pablo <juanpablosierra36@gmail.com>
2023-10-22 18:30:52 +0200

    Ejercicio tickets acabado  pero no resuelto.

--- src/ejercicios/unidad2/248tickets/248tickets.php
+++ src/ejercicios/unidad2/248tickets/248tickets.php
@@ -1,39 +1,4 @@
 <?php
-    //PROFESOR CODIGO
-    /*if(isset($_GET['cantidad'])){
-        $totalParametro = $_GET['cantidad'];
-    }
-    else{
-        $totalParametro = 500.50;
-    }
-
-    $arrayProductos = [
-        'cafe' => 1,
-        'tostada' => 1.50,
-        'coca-cola' => 2,
-        'cerveza' => 2,
-        'bravas' => 5,
-        'ensaladilla' => 7.5,
-        'pan' => 1,
-        'bocadillo' => 7.5
-    ];
-
-    function generarTickets(array $productos, float $total, int $tipoTicket = 1):array{
-
-        return [];
-    }
-
-    function permute($n, $str){
-        if($n == 1){
-            var_dump($str);
-        }
-    }
-    //foto function permute
-
-    //foto function pc_permute
-
-permute (count(array_values($arrayProductos)), $arrayProductos);*/
-
 /*
  * Nombre: Juan Pablo Sierra Amariles
  * Curso:  2ºDAW
@@ -54,46 +19,172 @@ permute (count(array_values($arrayProductos)), $arrayProductos);*/
 if (isset($_GET['tipoTicket'])) {
     $tipoTicket = $_GET['tipoTicket'];
 } else {
-    $tipoTicket = "El parámetro 'tipoTicket' no se pudo obtener.";
+    $tipoTicket = 1;
 }
 
 //Verificar limite
 if (isset($_GET['limite'])) {
     $limite = $_GET['limite'];
 } else {
-    $limite = "El parámetro 'limite' no se pudo obtener.";
+    $limite = 100;
 }
 
 $mercancia = [
-    'PanXOPE' => 3,
-    'Cola-Loca' => 1,
-    '1/3 Cerveza' => 1.5
+    'Cola Cao' => 1,
+    'Churros x2' => 1.50,
+    'Café' => 2,
+    'Zumo de naranja' => 2.20,
+    'Tostadas' => 2.5,
+    'Bocadillo' => 3.4,
+    'Batido' => 3.8,
+    'Tortitas x4' => 4
 ];
 
-$totalConsumido = 0;
+
 
 //FUNCIONES
-function generarNumeroSegunTicket(int $tipoTicket, array $mercancia): float{
-    switch ($tipoTicket) {
+
+function generarLimitePorTicket(float $tipoTicket, int $limite): float {
+    $limitePorTicket = 0;
+
+    switch ($tipoTicket){
         case 1:
-            $precio = random_int(1, 5);
-            /*foreach ($mercancia)*/
+            $limitePorTicket = random_int(1, 5);
+            break;
+
+        case 2:
+            $limitePorTicket = random_int(5, 10);
+            break;
+
+        case 3:
+            do {
+                $limitePorTicket = random_int(20, 70);
+            }while($limitePorTicket >= $limite);
+            break;
     }
 
-    return 0;
-}
-function generarTickets(array $mercancia, float $tipoTicket, int $limite ): array {
-    return 0;
+    return $limitePorTicket;
 }
 
+
+
+function sacarTotalServido(array $ticket):float{
+    $columna = 3; // Índice de la columna que deseas sumar
+    $total = 0;
+    foreach ($ticket as $fila) {
+        if (isset($fila[$columna])) { // Verifica si la columna existe en la fila
+            $total += $fila[$columna];
+        }
+    }
+
+    return $total;
+}
+
+//Esta función nos dirá si estamos cerca para llegar al limite del ticket.
+function quedaPoco($mercancia, $limite):bool{
+
+    foreach($mercancia as $precio){
+        if($precio <= $limite){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function generarTicket(array $mercancia, float $limitePorTicket):array{
+    $totalServido = 0;
+    $servido = [];
+
+    while($totalServido < $limitePorTicket){
+        $restante = $limitePorTicket - $totalServido;
+        $producto = array_rand($mercancia);
+        $precio = $mercancia[$producto];
+        $cantidad = ($precio < $restante) ? random_int(1, 3) : 1;
+
+        if(quedaPoco($mercancia, $restante)){
+            break;
+        }
+
+        if(($precio * $cantidad) <= $restante){
+            $totalServido += $precio * $cantidad;
+            $servido = [...$servido, [$producto, $precio, $cantidad, $precio * $cantidad]];
+        }
+    }
+
+    return $servido;
+}
+
+function generarTicketRestante($restante):array{
+    return [['Sobrante', $restante, 1, $restante]];
+}
+
+function establecerSobrante(int $tipoTicket, float $limite):float{
+
+    switch ($tipoTicket){
+        case 3:
+            return $limite * 0.10;
+            break;
+        default:
+            return $limite * 0.9;
+    }
+}
+
+function generarTickets(array $mercancia, int $tipoTicket, int $limite):array{
+    $tickets = [];
+    $totalTickets = 0;
+    $limitePorTicket = generarLimitePorTicket($tipoTicket, $limite);
+
+    while($totalTickets < $limite){
+        $restante = number_format($limite - $totalTickets, 2);
+
+        if(($limite - $totalTickets) < 1){
+            $ticketRestante = generarTicketRestante($restante);
+            $totalTickets += sacarTotalServido($ticketRestante);
+            $tickets = [...$tickets, $ticketRestante];
+        }
+        else{
+            $ticket = generarTicket($mercancia, $limitePorTicket);
+            $totalTickets += sacarTotalServido($ticket);
+            $tickets = [...$tickets, $ticket];
+        }
+    }
+
+    return $tickets;
+}
+
+function imprimirTickets(array $ticket, $numeroTicket) {
+    $salida = '<p><strong>TICKET #' . $numeroTicket . '</strong></p>';
+    $salida .= '<table border="1" style="width: 50%; border-collapse: collapse;">';
+    $salida.= '<tr><th>Producto</th><th>Precio</th><th>Cantidad</th><th>Total</th></tr>';
+
+    foreach ($ticket as $fila) {
+        $salida .= '<tr>';
+        foreach ($fila as $valor) {
+            $salida .= '<td>' . $valor . '</td>';
+        }
+        $salida .= '</tr>';
+    }
+
+    $salida .= '</table>';
+    return $salida;
+}
+
+$tickets = generarTickets($mercancia, $tipoTicket, $limite);
 
 //RESULTADO HTML
 $salida= "<html>
             <head><title>Ejercicio 248tickets</title></head>
-            <body>
-                <p>Tipo de Ticket = ".$tipoTicket."</p>   
-                <p>Limite = ".$limite."</p>           
-            </body>
+            <body>".
+                $numeroTicket = 1;
+                foreach ($tickets as $ticket) {
+                    $salida .= imprimirTickets($ticket, $numeroTicket);
+                    $numeroTicket++;
+                }
+
+                $salida .= "
+                <p>Precio Total: " . $limite . " euros</p>
+                </body>
             </html>";
 
 echo $salida;
